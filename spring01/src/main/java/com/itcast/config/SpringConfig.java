48174121570f6b0504a779438de96c92608c156c
AMchuAMchuAMchu <2466130840@qq.com>
2022-12-11 10:38:48 +0800

    shiina

--- spring01/src/main/java/com/itcast/config/SpringConfig.java
+++ spring01/src/main/java/com/itcast/config/SpringConfig.java
@@ -1,8 +1,11 @@
 package com.itcast.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
 
 /**
  * Description ==> TODO
